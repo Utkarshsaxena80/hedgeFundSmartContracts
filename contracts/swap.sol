9d5a25e012902cd6f3107b065174922f743febf8
Utkarsh Saxena <utkarshsaxena851@gmail.com>
2025-06-04 09:52:17 +0530

    another file

---
+++ contracts/swap.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "./hedgeToken.sol"; 
+
+contract Swap is Ownable {
+    HedgeToken public immutable i_token;
+
+    uint256 private totalETH;
+
+    constructor(address tokenAddress) Ownable(msg.sender) {
+        i_token = HedgeToken(tokenAddress);
+    }
+
+    function getToken() public  {
+        i_token.mint(address(this), 100000 ether);  
+    }
+
+    function getTokenStatus() public view returns (uint256) {
+        return i_token.balanceOf(address(this));
+    }
+
+    function receiveETHFromInvestors() public payable {
+        totalETH += msg.value;
+    }
+      function swap() 
+}
